2290bbde5bf3f2985504bc1705a3c64e72717aa8
sxyWxarder <drxlless@gmail.com>
2024-02-02 19:02:27 +0200

    Solution

--- app_anime_list/admin.py
+++ app_anime_list/admin.py
@@ -1,2 +1,11 @@
 from django.contrib import admin
+from .models import Anime, AnimeType
+
+class AnimeTypeAdmin(admin.ModelAdmin):
+    list_display = ('name', )
+    search_fields = ('name', )
+
+
+admin.site.register(Anime)
+admin.site.register(AnimeType, AnimeTypeAdmin)
 
