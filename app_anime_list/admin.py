6d0d7300123caf3cf890e2ecd266a376eda6b94f
sxyWxarder <drxlless@gmail.com>
2024-02-03 17:49:50 +0200

    sheyko

--- app_anime_list/admin.py
+++ app_anime_list/admin.py
@@ -5,7 +5,11 @@ class AnimeTypeAdmin(admin.ModelAdmin):
     list_display = ('name', )
     search_fields = ('name', )
 
+class AnimeAdmin(admin.ModelAdmin):
+    search_fields = ('name', )
+    list_display = ('name', 'my_rating', 'type', 'my_episode', 'episodes')
+    list_filter = ('my_rating', 'type', 'my_episode', 'episodes')
 
-admin.site.register(Anime)
+admin.site.register(Anime, AnimeAdmin)
 admin.site.register(AnimeType, AnimeTypeAdmin)
 
